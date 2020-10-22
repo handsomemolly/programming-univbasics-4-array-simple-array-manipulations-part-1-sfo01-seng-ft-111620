e81429551819f9cf407aa55fabd755e6b34cf6fb
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-22 20:40:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array, item1, item2)
+  array.pop(item1, item2)
+end
